caa8bb6541e35809f5cc4611d6649f95859cda8e
nabinhere <nabindrashrestha10@gmail.com>
2024-09-05 14:28:17 +0545

    corrected the original dataframes code to add all buildings

--- predict_power.py
+++ predict_power.py
@@ -6,11 +6,29 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
+PHYSICS = 0
+BIOTECH = 1
+MANAGEMENT = 2
+CIVIL = 3
+
 weekday_encoding = {'Sunday': 1, 'Monday': 2, 'Tuesday': 3, 'Wednesday': 4, 'Thursday': 5, 'Friday': 6, 'Saturday': 7}
 
-dataset_PATH = r"C:\Users\nabin\OneDrive\Desktop\AI Gridview\[0] Datasets\Biotech_preprocessed_dataset_with_outliers.csv"
-original_df = pd.read_csv(dataset_PATH)
-original_df = original_df[['Hour', 'Day', 'Total (W)']]
+dataset_root_PATH = r"C:\Users\nabin\OneDrive\Desktop\AI Gridview\[0] Datasets"
+dataset_PATH = {PHYSICS: dataset_root_PATH + r"Physics_preprocessed_dataset_with_outliers",
+                BIOTECH: dataset_root_PATH + r"Biotech_preprocessed_dataset_with_outliers",
+                MANAGEMENT: dataset_root_PATH + r"Management_preprocessed_dataset_with_outliers",
+                CIVIL: dataset_root_PATH + r"Civil_preprocessed_dataset_with_outliers"}
+
+# get original dataframes
+original_df = {PHYSICS: pd.read_csv(dataset_PATH[PHYSICS]),
+                BIOTECH: pd.read_csv(dataset_PATH[BIOTECH]),
+                MANAGEMENT: pd.read_csv(dataset_PATH[MANAGEMENT]),
+                CIVIL: pd.read_csv(dataset_PATH[CIVIL])}
+# get only the required columns from each dataframe
+original_df = {PHYSICS: original_df[PHYSICS][['Hour', 'Day', 'Total (W)']],
+                BIOTECH: original_df[BIOTECH][['Hour', 'Day', 'Total (W)']],
+                MANAGEMENT: original_df[MANAGEMENT][['Hour', 'Day', 'Total (W)']],
+                CIVIL: original_df[CIVIL][['Hour', 'Day', 'Total (W)']]}
 
 # model class
 class LSTM(nn.Module):
@@ -41,10 +59,6 @@ class CNNLSTM(nn.Module):
     out = self.fc(out)
     return out
   
-PHYSICS = 0
-BIOTECH = 1
-MANAGEMENT = 2
-CIVIL = 3
 
 # load model
 PATH = {PHYSICS:'physics_cnnlstm_mip.pth', 
@@ -74,8 +88,8 @@ def get_predicted_power(total_power, building:int):
   input_dict = {"Hour": [Hour], "Day": [Day], "Total (W)": [total_power]}
   input_df = pd.DataFrame(input_dict)
 
-  new_df = pd.concat([original_df, input_df], ignore_index=True)
-  new_df['Day'] = new_df['Day'].replace(weekday_encoding)
+  new_df = pd.concat([original_df[building], input_df], ignore_index=True)
+  new_df['Day'] = new_df['Day'].map(weekday_encoding)
   columns_to_scale = ['Hour', 'Day', 'Total (W)']
   scaler = MinMaxScaler(feature_range=(-1, 1))
   new_df[columns_to_scale] = scaler.fit_transform(new_df[columns_to_scale])
@@ -98,7 +112,6 @@ def get_predicted_power(total_power, building:int):
       predicted_total_power = np.repeat(predicted_total_power, 3, axis=1)
       predicted_total_power = scaler.inverse_transform(predicted_total_power)
       predicted_total_power = predicted_total_power[:, 2]
-      # print(timestamp)
 
 
   return int(predicted_total_power[0])
